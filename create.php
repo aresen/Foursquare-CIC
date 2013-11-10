d2c9beddf822e5a71d9f095546b3fa19764c9e96
Quentin Li <quentin.li147@gmail.com>
2013-11-10 00:13:52 -0500

    Progress on create

--- create.php
+++ create.php
@@ -1,42 +1,42 @@
 <?php
 include_once "config.php";
-if($_GET['code']{
-$authkey = file_get_contents("https://foursquare.com/oauth2/access_token?client_id=".$client_id."&client_secret=".$secret."&grant_type=authorization_code&redirect_uri=".$redirect."&code=".$_GET['code']);
-$decoded_auth = json_decode($authkey,true);
-$access_token = $decoded_auth['access_token'];
-
-$userinfo = file_get_contents("https://api.foursquare.com/v2/users/self?oauth_token=".$access_token);
-$decoded_userinfo= json_decode($userinfo, true);
-$name = $decoded_userinfo['response']['user']['firstName'];
-
-$auto_complete = file_get_contents("https://api.foursquare.com/v2/venues/suggestCompletion".$access_token);
-
-
-}
+session_start();
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 	<title>Create!</title>
-  	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  	<script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places"></script>
-	<script src="jquery.geocomplete.js"></script>
-	<script $ ("input").geocomplete({ details: "form" });</script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>
+	$( document ).ready(function(){
+	    var oauth = encodeURI('<?php echo $_SESSION['access_token']; ?>');
+	    console.log("LOLOL");
+	    $("#test").click(function() {
+		var place = $("#place").val();
+		var keyw = $("#keyw").val();
+		var venueSearch = "https://api.foursquare.com/v2/venues/search&intent=checkin&near="+encodeURIComponent(place)+"&query="+encodeURIComponent(keyw)+"&oauth_token="+oauth; 
+	        $.getJSON(venueSearch, function(){
+		}) 
+		.done(function (id, name) {
+		console.log(id, name);
+		})
+	    }); 
+	});
+	</script>
 </head>
  
     <body>
     <div id="content">
     	<h1> Let's play a game... </h1>
-    	<p><Add a place!</p>
-    	<form>
-    		Place: <input name="venue" type="text" value="">
-    		Latitude: <input name="lng" type="text" value="">
-    		Longitude: <input name="long" type="text" value="">
+    	<form action="submit.php" method="post">
+    		Keyword: <input name="venue" type="text" value="" id="keyw">
+    		Place: <input name="place" type="text" value="" id="place">
+	<input name="submit" type="button" value ="submit" id='test'>
     	</form>
+    	<p>Add a place!</p>
+	<br>
     </div>
     </body>
  </html>
